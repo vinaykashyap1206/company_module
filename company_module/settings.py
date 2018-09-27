1ff24200fc55ca553b5ca522b764c12cb470689b
vinay <vinaykashyap1206@gmail.com>
2018-09-27 20:03:56 +0530

    update host into settings

--- company_module/settings.py
+++ company_module/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4dx7f*=44l-46by+*1&sv05$fapl_g!tygr$sly+nvilhn9)x-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
